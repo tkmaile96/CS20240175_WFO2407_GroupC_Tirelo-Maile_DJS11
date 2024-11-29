de6876f5cd847c731913277c43ce23fd946815eb
Teekay <tkmaile96@gmail.com>
2024-11-29 03:59:30 +0200

    Updated Styles in global.css

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';// import useState and useEffect from the React library
 import { Link } from 'react-router-dom';// import Link from the react-router-dom library
 import PodcastImage from '../src/assets/podcast.png'; // podacst image
+import Banner from './assets/Podcast background.jpg'; // banner image
 
 
 
